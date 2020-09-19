66a7a3dc97628733467c8dbc3ab543847b4a7087
KirillIvano <ivannadez@yandex.ru>
2020-09-20 01:24:41 +0300

    update common styles and create iframe connection

--- common/index.js
+++ common/index.js
@@ -1,2 +1,57 @@
-import './styles.sass';
+import './styles.scss';
+import './vars.scss';
 
+
+const HEIGHT_CHANGE_EVENT = 'iframe_height';
+
+
+const getArticleHeight = () => document.body.scrollHeight;
+
+const createHeightMessage = height => ({
+    __app__: true,
+    payload: {
+        type: HEIGHT_CHANGE_EVENT,
+        data: {
+            height,
+        },
+    },
+});
+
+const sendHeightMessage = height => window.parent
+    .postMessage(createHeightMessage(height), '*');
+
+
+const createHeightObservable = (tickInterval=200) => {
+    const handlers = [];
+    let prevHeight = getArticleHeight();
+
+    const handleTick = () => {
+        const currentHeight = getArticleHeight();
+
+        if (currentHeight !== prevHeight) {
+            prevHeight = currentHeight;
+            handlers.forEach(f => f(currentHeight));
+        }
+    };
+
+    const tickerId = setInterval(handleTick, tickInterval);
+
+    return ({
+        observe: f => handlers.push(f),
+        unobserve: f => {
+            const handlerInd = handlers.indexOf(f);
+            handlerInd !== -1 && handlers.splice(handlerInd, 1);
+        },
+        destroy: () => clearInterval(tickerId),
+    });
+};
+
+
+const init = () => {
+    const currentHeight = getArticleHeight();
+    sendHeightMessage(currentHeight);
+
+    createHeightObservable().observe(sendHeightMessage);
+};
+
+init();
